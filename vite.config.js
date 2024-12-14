0e4aae9dc8ea9c7566fb086086cb6ea4f3f58a4a
mxnv03 <maximn11022003@gmail.com>
2024-12-14 23:18:54 +0300

    t-2: [+] all: deploy to github

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/grand-amak.github.io/',
-  build: {
-    outDir: 'dist',
-  },
-});
+  base: '/grand-amak',
+  plugins: [react()],
+})
